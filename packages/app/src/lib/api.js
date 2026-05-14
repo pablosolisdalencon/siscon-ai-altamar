669c53df6e1955c43a3d92f3150a4ba2859d3b1a
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2026-05-13 23:08:59 -0400

    Update api.js

--- packages/app/src/lib/api.js
+++ packages/app/src/lib/api.js
@@ -1,10 +1,38 @@
 import axios from 'axios';
 
+// Add interceptor to global axios to cover direct imports like in Empresa.jsx
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000/api',
   headers: {
     'Content-Type': 'application/json'
   }
 });
+
+// Also add to the custom instance just in case
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
